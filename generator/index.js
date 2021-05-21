175210f09358149038641a06f993fe5477327156
凌柏超 <1585638808@qq.com>
2021-05-21 18:26:23 +0800

    init

---
+++ generator/index.js
@@ -0,0 +1,61 @@
+function renderFiles (api, opts) {
+
+  const fs = require('fs')
+
+  // 通过preset的形式配置opts.router，这里则不需要
+  const routerPath = api.resolve('./src/router.js')
+  opts.router = opts.router || fs.existsSync(routerPath)
+
+  const filesToDelete = [
+    'src/assets/logo.png',
+    'src/views/About.vue',
+    'src/views/Home.vue',
+  ]
+
+  console.log('\n[custom-tpl plugin tips]\n \t GeneratorAPI options:', opts)
+
+  if (opts.replaceTemplates) {
+
+    // https://github.com/vuejs/vue-cli/issues/2470
+    api.render(files => {
+      Object.keys(files)
+        .filter(name => filesToDelete.indexOf(name) > -1)
+        .forEach(name => delete files[name])
+    })
+
+    api.render('./templates/base')
+
+    if (opts.router) {
+      // 替换掉路由文件
+      api.render('./templates/sp')
+    }else{
+    }
+
+  }
+}
+
+function addDependencies (api) {
+  api.extendPackage({
+    dependencies: {
+      "axios": "^0.18.0",
+    },
+    devDependencies: {
+      // "less": "^3.0.4",
+      // "less-loader": "^4.1.0",
+    }
+  })
+}
+
+module.exports = (api, opts, rootOpts) => {
+
+  api.extendPackage({
+    scripts: {
+      // 测试环境构建，此时NODE_ENV='test_production'
+      test_build: `vue-cli-service build --mode test_production`
+    }
+  })
+
+  addDependencies(api)
+
+  renderFiles(api, opts)
+}
